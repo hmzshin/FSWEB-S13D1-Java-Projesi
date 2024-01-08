63766d6643ff79c4794c630e88e654c1a96233d3
hmzshin <hamzasahin@outlook.com>
2024-01-08 12:11:10 +0300

    görev2: Yaş tespiti metodu tanımlandı

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,25 @@ public class Main {
         System.out.println("shouldWakeUp: " + shouldWakeUp(true, 8));
         System.out.println("shouldWakeUp: " + shouldWakeUp(true, -1));
 
+        System.out.println("Has teen: " + hasTeen(9, 99, 19));
+        System.out.println("Has teen: " + hasTeen(23, 15, 42));
+        System.out.println("Has teen: " + hasTeen(22, 23, 34));
+
     }
 
     public static boolean shouldWakeUp(boolean isDogBarking, int time) {
         return ((time >= 0 && time < 8) || (time >= 20 && time < 24)) && isDogBarking;
     }
 
-        System.out.println("Hello world!");
+    public static boolean hasTeen(int age1, int age2, int age3) {
+
+        int[] ages = { age1, age2, age3 };
+        for (int age : ages) {
+            if (age >= 13 && age <= 19) {
+                return true;
+            }
+        }
+        return false;
 
     }
 }
