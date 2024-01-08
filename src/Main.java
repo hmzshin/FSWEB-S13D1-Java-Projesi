c324fdbd3461758dbdf6dca42938e3c4d963494d
hmzshin <hamzasahin@outlook.com>
2024-01-08 12:10:18 +0300

    görev1: Havlayan köpek metodu tanımlandı

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("shouldWakeUp: " + shouldWakeUp(true, 1));
+        System.out.println("shouldWakeUp: " + shouldWakeUp(false, 2));
+        System.out.println("shouldWakeUp: " + shouldWakeUp(true, 8));
+        System.out.println("shouldWakeUp: " + shouldWakeUp(true, -1));
+
+    }
+
+    public static boolean shouldWakeUp(boolean isDogBarking, int time) {
+        return ((time >= 0 && time < 8) || (time >= 20 && time < 24)) && isDogBarking;
+    }
+
         System.out.println("Hello world!");
+
     }
 }
