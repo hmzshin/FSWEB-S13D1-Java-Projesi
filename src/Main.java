3f0786763acc5655f1402903810fac1aff0cb505
hmzshin <hamzasahin@outlook.com>
2024-01-08 13:35:04 +0300

    görev4: Alan Hesaplama metodunu tanımlar

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
         System.out.println("shouldWakeUp: " + shouldWakeUp(true, 1));
@@ -9,17 +10,20 @@ public class Main {
         System.out.println("Has teen: " + hasTeen(23, 15, 42));
         System.out.println("Has teen: " + hasTeen(22, 23, 34));
 
-
         System.out.println("Is cat playing: " + isCatPlaying(true, 10));
         System.out.println("Is cat playing: " + isCatPlaying(false, 36));
         System.out.println("Is cat playing: " + isCatPlaying(false, 35));
 
+        System.out.println("Area of rectangle: " + areaRectangle(5.0,4.0));
+        System.out.println("Area of rectangle: " + areaRectangle(-1.0,4.0));
+
+        System.out.println("Area of circle: " + areaCircle(5.0));
+        System.out.println("Area of circle: " + areaCircle(-1.0));
     }
 
     public static boolean shouldWakeUp(boolean isDogBarking, int time) {
         return ((time >= 0 && time < 8) || (time >= 20 && time < 24)) && isDogBarking;
     }
-
     public static boolean hasTeen(int age1, int age2, int age3) {
 
         int[] ages = { age1, age2, age3 };
@@ -31,13 +35,24 @@ public class Main {
         return false;
 
     }
-
     public static boolean isCatPlaying(boolean isSummer, int temperature) {
-        if(isSummer){
+        if (isSummer) {
             return temperature >= 25 && temperature <= 45;
-        }else{
-
+        } else {
             return temperature >= 25 && temperature <= 35;
         }
+    }
+
+    public static double areaRectangle(double a, double b) {
+        if (a <= 0 || b <= 0) {
+            return -1;
+        }
+        return a*b;
+    }
+    public static double areaCircle(double radius) {
+        if (radius <= 0) {
+            return -1;
+        }
+        return Math.PI*radius*radius;
     }
 }
